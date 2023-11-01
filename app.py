e17e78935eb40f6ced0a088fd942572b1918594d
midorigameee <xqbodp727@gmail.com>
2023-11-02 00:27:28 +0900

    ファイルアップロード機能を追加
    参考:https://progmemo.hatenablog.jp/entry/2022/07/19/020830

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+import os
+
 
 app = Flask(__name__)
 
@@ -8,6 +10,24 @@ def index():
         "index.html",
         title="Top Page"
         )
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    # URLでhttp://127.0.0.1:5000/uploadを指定したときはGETリクエストとなるのでこっち
+    if request.method == 'GET':
+        return render_template('upload.html')
+    # formでsubmitボタンが押されるとPOSTリクエストとなるのでこっち
+    elif request.method == 'POST':
+        file = request.files['example']
+        file.save(os.path.join('static', 'images', file.filename))
+        return redirect(url_for('uploaded_file', filename=file.filename))
+
+
+@app.route('/uploaded_file/<string:filename>')
+def uploaded_file(filename):
+    return render_template('uploaded_file.html', filename=filename)
+
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0')
