033afc4b75a4981bc819c184ddd236febeab4f3e
midorigameee <xqbodp727@gmail.com>
2023-11-01 23:55:23 +0900

    templateの実装

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Python x Flask"
+    return render_template(
+        "index.html",
+        title="Top Page"
+        )
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0')
